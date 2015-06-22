02aedd74cd56f8cfb03733a6df16c8460f7cd324
wanghao <odaywanghao@gmail.com>
2015-06-22 17:48:28 +0800

    完成Admin模块构建,实现基本的background信息,修复HOME模块的一点问题

---
+++ Application/Runtime/Cache/Admin/1e91f8dbf6ab5053f684934e7abeaef2.php
@@ -0,0 +1,48 @@
+<?php if (!defined('THINK_PATH')) exit();?><h1>Users </h1><br/>
+userCount:<?php echo ($userCount); ?>
+<h2>firstUser</h2><br/>
+username:<?php echo ($userAll[0][username]); ?><br/>
+password:<?php echo ($userAll[0][password]); ?><br/>
+email:<?php echo ($userAll[0][email]); ?><br/>
+
+<h1>Notes</h1><br/>
+noteAllCount:<?php echo ($noteAllCount); ?><br/>
+nowTime:<?php echo ($nowTime); ?><br/>
+noteLast1DayCount:<?php echo ($noteCount[day]); ?><br/>
+noteLast1WeekCount:<?php echo ($noteCount[week]); ?><br/>
+noteLast1WeelCount:<?php echo ($noteCount[month]); ?><br/>
+
+<h1>books</h1><br/>
+bookAllCount:<?php echo ($bookAllCount); ?><br/>
+nowTime:<?php echo ($nowTime); ?><br/>
+bookLast1DayCount:<?php echo ($bookCount[day]); ?><br/>
+bookLast1WeekCount:<?php echo ($bookCount[week]); ?><br/>
+bookLast1WeelCount:<?php echo ($bookCount[month]); ?><br/>
+
+
+
+
+
+<h1>comments</h1><br/>
+commentAllCount:<?php echo ($commentAllCount); ?><br/>
+nowTime:<?php echo ($nowTime); ?><br/>
+commentLast1DayCount:<?php echo ($commentCount[day]); ?><br/>
+commentLast1WeekCount:<?php echo ($commentCount[week]); ?><br/>
+commentLast1WeelCount:<?php echo ($commentCount[month]); ?><br/>
+
+
+<h1>note everyCount</h1>
+1:<?php echo ($noteEveryCount[1]); ?><br/>
+2:<?php echo ($noteEveryCount[2]); ?><br/>
+3:<?php echo ($noteEveryCount[3]); ?><br/>
+4:<?php echo ($noteEveryCount[4]); ?><br/>
+5:<?php echo ($noteEveryCount[5]); ?><br/>
+6:<?php echo ($noteEveryCount[6]); ?><br/>
+7:<?php echo ($noteEveryCount[7]); ?><br/>
+8:<?php echo ($noteEveryCount[8]); ?><br/>
+9:<?php echo ($noteEveryCount[9]); ?><br/>
+10:<?php echo ($noteEveryCount[10]); ?><br/>
+11:<?php echo ($noteEveryCount[11]); ?><br/>
+12:<?php echo ($noteEveryCount[12]); ?><br/>
+13:<?php echo ($noteEveryCount[13]); ?><br/>
+<br/>
